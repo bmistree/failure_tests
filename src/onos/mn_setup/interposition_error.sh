7c1bd541f265db5f2323b00511d7657302c32cc2
bmistree <bmistree@gmail.com>
2015-01-08 16:01:33 -0800

    Added script for starting interposer that issues errors.

---
+++ src/onos/mn_setup/interposition_error.sh
@@ -0,0 +1,11 @@
+
+#!/usr/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    printf "\nPlease specify path to interposition.py "
+    printf "(including interposition.py).  This will "
+    printf "likely be in the deps folder sdn_fuzz/bin.\n"
+else
+    # provided path to interposition.
+    python $1 --type=error --listen-on-addr=127.0.0.1:8888 --controller-addr=127.0.0.1:6633 --additional='{"failure_probability": .2}'
+fi
